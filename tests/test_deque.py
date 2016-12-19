fb5646977a96d4b10eb102b5aca1603267d572d3
Avery Pratt <apratt91@gmail.com>
2016-12-19 13:46:21 -0800

    set up scaffolding for tests and deque

---
+++ tests/test_deque.py
@@ -0,0 +1,26 @@
+"""Tests methods on Deque object."""
+
+import pytest
+from src.deque import Deque
+
+
+def test_append():
+    pass
+
+def test_appendleft():
+    pass
+
+def test_pop():
+    pass
+
+def test_popleft():
+    pass
+
+def test_peek():
+    pass
+
+def test_peekleft():
+    pass
+
+def test_size():
+    pass
