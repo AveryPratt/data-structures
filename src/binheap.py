bd9e10209ba594780cf29da81e8349e4f481f84e
Avery Pratt <apratt91@gmail.com>
2016-12-21 22:55:21 -0800

    still debugging

--- src/binheap.py
+++ src/binheap.py
@@ -12,31 +12,29 @@ class BinHeap(object):
     and fills in the remaining space with the smallest number available.
     """
 
-    def __init__(self, lst=[]):
+    def __init__(self, lst=None):
         """Creates an instance of a binary min-heap using a list."""
+        self.lst = []
         for val in lst:
-            if not isinstance(val, int) and not isinstance(val, float):
-                raise TypeError("Cannot create a binary heap out of a list " +
-                                "that contains non-numerical members.")
-        self.lst = lst
+            self.push(val)
+
 
     def push(self, val):
         """Adds a value to the last index on the heap, and then sorts the heap."""
         if not isinstance(val, int) and not isinstance(val, float):
             raise TypeError("Cannot add a non-numerical member to a binary heap.")
         self.lst.append(val)
+        self._sink(self.lst[-1])
 
 
     def pop(self):
         """Removes the value at the first index on the heap, and then sorts the heap."""
+        if len(self.lst) < 1:
+            raise IndexError("Cannot pop from an empty heap.")
+        val = self.lst[0]
         self.lst[0] = None
         self._bubble()
-        for ind in range(len(self.lst) // 2, len(self.lst) - 1):
-            if self.lst[ind] is None:
-                self.lst[ind] = self.lst[ind + 1]
-        self.lst = self.lst[:-1]
-        for ind in range(len(self.lst) // 2 - 1, -1, -1):
-            self._check_swap(ind)
+        return val
 
 
     def _check_swap(self, ind):
@@ -45,28 +43,31 @@ class BinHeap(object):
         second = first + 1
         try:
             lesser_ind = first if self.lst[first] <= self.lst[second] else second
-        except AttributeError:
+        except IndexError:
             lesser_ind = self.lst[first]
         if self.lst[lesser_ind] < self.lst[ind] or self.lst[ind] is None:
             self.lst[ind], self.lst[lesser_ind] = self.lst[lesser_ind], self.lst[ind]
             return lesser_ind
+        return None
+
+
+    def _bubble(self):
+        if len(self.lst) > 1:
+            pointer = 0
+            while pointer is not None and pointer < len(self.lst) // 2:
+                pointer = self._check_swap(pointer)
+            for ind in range(pointer, len(self.lst) - 1):
+                self.lst[ind] = self.lst[ind + 1]
+            self.lst = self.lst[-1]
+            return True
         return False
 
 
-    def _bubble(self, pointer=0):
-        while pointer < len(self.lst) // 2:
-            pointer = self._check_swap(pointer)
-
-
-    def _sink(self, pointer=None):
-        if pointer is None:
-            pointer = len(self.lst) // 2 - 1
-
-
-    def _cascade(self):
-        cascade_ind = len(self.lst) // 2 - 1
-        while True:
-            swapped = self._check_swap(cascade_ind)
-            cascade_ind = (cascade_ind - 1) // 2
-            if not swapped:
-                break
+    def _sink(self, pointer):
+        if len(self.lst) > 1 and pointer >= 0 and pointer < len(self.lst):
+            swapped = 0
+            while swapped is not None:
+                pointer = (pointer - 1) // 2
+                swapped = self._check_swap(pointer)
+            return True
+        return False
--- src/tests/test_binheap.py
+++ src/tests/test_binheap.py
@@ -5,19 +5,19 @@ import pytest
 
 
 LST = [45, -93, 68, 39, 71, -99, -37, 12, 8, -38, 104, 38, 55, -10, 30, -45, 78, 0, -34, -10]
-SORTED = []
+SORTED = [-99, -93, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
 
 
 @pytest.fixture
 def emptybh():
-    """Creates an empty Deque for testing."""
+    """Creates an empty BinHeap for testing."""
     from src.binheap import BinHeap
     return BinHeap()
 
 
 @pytest.fixture
 def fullbh():
-    """Creates an empty Deque for testing."""
+    """Creates a BinHeap for testing with LST as values."""
     from src.binheap import BinHeap
     return BinHeap(LST)
 
@@ -27,21 +27,146 @@ def test_constructor1(emptybh):
 
 
 def test_constructor2(fullbh):
-    assert fullbh == LST
+    assert fullbh == SORTED
 
 
-def test_sortheap1(emptybh):
-    emptybh._sortheap()
+def test_check_swap2(fullbh):
+    fullbh._check_swap(0)
+    assert fullbh.lst == SORTED
+
+
+def test_check_swap3(fullbh):
+    fullbh.lst[0] = 0
+    fullbh._check_swap(0)
+    assert fullbh.lst == [-93, 0, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+
+
+def test_check_swap4(fullbh):
+    fullbh._check_swap(3)
+    assert fullbh.lst == SORTED
+
+
+def test_check_swap5(fullbh):
+    fullbh.lst[3] = 1
+    fullbh._check_swap(3)
+    assert fullbh.lst == [-99, -93, 45, -34, -38, 68, -37, 39, 1, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+
+
+def test_check_swap6(fullbh):
+    fullbh._check_swap(9)
+    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, -10, 104, 38, 55, -10, 30, 12, 78, 0, 8, 71]
+
+
+def test_bubble1(emptybh):
+    emptybh._bubble()
     assert emptybh.lst == []
 
 
-def test_sortheap2(fullbh):
-    fullbh._sortheap()
-    assert fullbh.lst == LST
+def test_bubble2(fullbh):
+    fullbh._bubble()
+    assert fullbh.lst == SORTED
 
 
-def test_push():
-    pass
+def test_bubble3(fullbh):
+    fullbh.lst[0] = 100
+    fullbh._bubble()
+    assert fullbh.lst == [-93, -45, 45, -34, -38, 68, -37, 39, 0, 71, 104, 38, 55, -10, 30, 12, 78, 100, 8, -10]
 
-def test_pop():
-    pass
+
+def test_bubble4(fullbh):
+    fullbh.lst[0] = -50
+    fullbh._bubble()
+    assert fullbh.lst == [-93, -50, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+
+
+def test_bubble5(fullbh):
+    fullbh._bubble(9)
+    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, -10, 104, 38, 55, -10, 30, 12, 78, 0, 8, 71]
+
+
+def test_bubble6(fullbh):
+    fullbh._bubble(10)
+    assert fullbh.lst == SORTED
+
+
+def test_bubble7(fullbh):
+    fullbh._bubble(-1)
+    assert fullbh.lst == SORTED
+
+
+def test_sink1(emptybh):
+    emptybh._sink(0)
+    assert emptybh.lst == []
+
+
+def test_sink2(emptybh):
+    emptybh.lst.append(1)
+    emptybh._sink()
+    assert emptybh.lst == [1]
+
+
+def test_sink3(fullbh):
+    fullbh.lst.append(-100)
+    fullbh._sink()
+    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+
+
+def test_sink4(fullbh):
+    fullbh.lst.append(0)
+    fullbh._sink()
+    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+
+
+def test_sink5(fullbh):
+    fullbh.lst[6] = -100
+    fullbh._sink(6)
+    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+
+
+def test_pop1(emptybh):
+    with pytest.raises(IndexError):
+        emptybh.pop()
+
+
+def test_pop2():
+    from src.binheap import BinHeap
+    binheap = BinHeap([5])
+    assert binheap.pop() == 5
+
+
+def test_pop3(fullbh):
+    assert fullbh.pop() == -99
+
+
+def test_pop4(fullbh):
+    len_lst = len(fullbh.lst)
+    fullbh.pop()
+    assert len(fullbh.lst) == len_lst - 1
+
+
+def test_pop5(fullbh):
+    sorted_lst = LST
+    sorted_lst.sort()
+    passes = True
+    for item in sorted_lst:
+        match = fullbh.pop()
+        assert item != match
+        # if item != match:
+        #     passes = False
+        #     break
+    # assert passes
+
+
+def test_push1(emptybh):
+    emptybh.push(5)
+    assert len(emptybh.lst) == 1
+
+
+def test_push2(emptybh):
+    emptybh.push(5)
+    assert emptybh.lst[0] == 5
+
+
+def test_push3(emptybh):
+    with pytest.raises(TypeError):
+        emptybh.push("hello")
