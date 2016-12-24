e99f2ff761e43cba9e67a4381412a5ca8bc48420
Avery Pratt <apratt91@gmail.com>
2016-12-23 19:59:13 -0800

    binary heap now passing all tests

--- src/binheap.py
+++ src/binheap.py
@@ -8,66 +8,49 @@ class BinHeap(object):
     push(val): adds a value to the last index on the heap,
     and throws a type error if the value is not an int or float.
 
-    pop(): removes the value in the first index (top) of the heap,
+    pop(): removes the value in the first index of the heap,
     and fills in the remaining space with the smallest number available.
     """
-
     def __init__(self, lst=None):
         """Creates an instance of a binary min-heap using a list."""
-        self.lst = []
+        self._lst = []
+        if lst == None:
+            lst = []
         for val in lst:
             self.push(val)
 
-
     def push(self, val):
         """Adds a value to the last index on the heap, and then sorts the heap."""
-        if not isinstance(val, int) and not isinstance(val, float):
-            raise TypeError("Cannot add a non-numerical member to a binary heap.")
-        self.lst.append(val)
-        self._sink(self.lst[-1])
-
+        self._lst.append(val)
+        self._sink(len(self._lst) - 1)
 
     def pop(self):
         """Removes the value at the first index on the heap, and then sorts the heap."""
-        if len(self.lst) < 1:
-            raise IndexError("Cannot pop from an empty heap.")
-        val = self.lst[0]
-        self.lst[0] = None
-        self._bubble()
+        val = self._lst[0]
+        self._lst[0] = self._lst[len(self._lst) - 1]
+        self._bubble(0)
+        self._lst = self._lst[:-1]
         return val
 
-
-    def _check_swap(self, ind):
-        """Checks whether a node can be swapped with one of it's child nodes"""
-        first = 2 * ind + 1
-        second = first + 1
+    def _bubble(self, ind):
+        """Pushes value at ind up the heap until both of it's children's values are greater."""
+        first_child = (2 * ind) + 1
+        if first_child >= len(self._lst):
+            return
+        second_child = first_child + 1
         try:
-            lesser_ind = first if self.lst[first] <= self.lst[second] else second
+            child_ind = first_child if self._lst[first_child] < self._lst[second_child] else second_child
         except IndexError:
-            lesser_ind = self.lst[first]
-        if self.lst[lesser_ind] < self.lst[ind] or self.lst[ind] is None:
-            self.lst[ind], self.lst[lesser_ind] = self.lst[lesser_ind], self.lst[ind]
-            return lesser_ind
-        return None
+            child_ind = first_child
+        if self._lst[ind] > self._lst[child_ind]:
+            self._lst[ind], self._lst[child_ind] = self._lst[child_ind], self._lst[ind]
+            self._bubble(child_ind)
 
-
-    def _bubble(self):
-        if len(self.lst) > 1:
-            pointer = 0
-            while pointer is not None and pointer < len(self.lst) // 2:
-                pointer = self._check_swap(pointer)
-            for ind in range(pointer, len(self.lst) - 1):
-                self.lst[ind] = self.lst[ind + 1]
-            self.lst = self.lst[-1]
-            return True
-        return False
-
-
-    def _sink(self, pointer):
-        if len(self.lst) > 1 and pointer >= 0 and pointer < len(self.lst):
-            swapped = 0
-            while swapped is not None:
-                pointer = (pointer - 1) // 2
-                swapped = self._check_swap(pointer)
-            return True
-        return False
+    def _sink(self, ind):
+        """Pushes value down the heap until it's parent's value is less."""
+        if ind == 0:
+            return
+        parent_ind = (ind - 1) // 2
+        if self._lst[ind] < self._lst[parent_ind]:
+            self._lst[ind], self._lst[parent_ind] = self._lst[parent_ind], self._lst[ind]
+            self._sink(parent_ind)
--- src/tests/test_binheap.py
+++ src/tests/test_binheap.py
@@ -4,8 +4,8 @@
 import pytest
 
 
-LST = [45, -93, 68, 39, 71, -99, -37, 12, 8, -38, 104, 38, 55, -10, 30, -45, 78, 0, -34, -10]
-SORTED = [-99, -93, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+START = [45, -93, 68, 39, 71, -99, -37, 12, 8, -38, 104, 38, 55, -10, 30, -45, 78, 0, -34, -10]
+SORTED = [-99, -45, -93, -38, -10, 38, -37, 12, -34, 8, 104, 68, 55, -10, 30, 45, 78, 39, 0, 71]
 
 
 @pytest.fixture
@@ -17,110 +17,78 @@ def emptybh():
 
 @pytest.fixture
 def fullbh():
-    """Creates a BinHeap for testing with LST as values."""
+    """Creates a BinHeap for testing with heap as values."""
     from src.binheap import BinHeap
-    return BinHeap(LST)
+    return BinHeap(START)
 
 
 def test_constructor1(emptybh):
-    assert emptybh.lst == []
+    assert emptybh._lst == []
 
 
 def test_constructor2(fullbh):
-    assert fullbh == SORTED
-
-
-def test_check_swap2(fullbh):
-    fullbh._check_swap(0)
-    assert fullbh.lst == SORTED
-
-
-def test_check_swap3(fullbh):
-    fullbh.lst[0] = 0
-    fullbh._check_swap(0)
-    assert fullbh.lst == [-93, 0, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
-
-
-def test_check_swap4(fullbh):
-    fullbh._check_swap(3)
-    assert fullbh.lst == SORTED
-
-
-def test_check_swap5(fullbh):
-    fullbh.lst[3] = 1
-    fullbh._check_swap(3)
-    assert fullbh.lst == [-99, -93, 45, -34, -38, 68, -37, 39, 1, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
-
-
-def test_check_swap6(fullbh):
-    fullbh._check_swap(9)
-    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, -10, 104, 38, 55, -10, 30, 12, 78, 0, 8, 71]
+    assert fullbh._lst == SORTED
 
 
 def test_bubble1(emptybh):
-    emptybh._bubble()
-    assert emptybh.lst == []
+    emptybh._bubble(0)
+    assert emptybh._lst == []
 
 
 def test_bubble2(fullbh):
-    fullbh._bubble()
-    assert fullbh.lst == SORTED
+    fullbh._bubble(0)
+    assert fullbh._lst == SORTED
 
 
 def test_bubble3(fullbh):
-    fullbh.lst[0] = 100
-    fullbh._bubble()
-    assert fullbh.lst == [-93, -45, 45, -34, -38, 68, -37, 39, 0, 71, 104, 38, 55, -10, 30, 12, 78, 100, 8, -10]
+    fullbh._lst[0] = 100
+    fullbh._bubble(0)
+    assert fullbh._lst == [-93, -45, -37, -38, -10, 38, -10, 12, -34, 8, 104, 68, 55, 100, 30, 45, 78, 39, 0, 71]
 
 
 def test_bubble4(fullbh):
-    fullbh.lst[0] = -50
-    fullbh._bubble()
-    assert fullbh.lst == [-93, -50, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+    fullbh._lst[0] = -50
+    fullbh._bubble(0)
+    assert fullbh._lst == [-93, -45, -50, -38, -10, 38, -37, 12, -34, 8, 104, 68, 55, -10, 30, 45, 78, 39, 0, 71]
 
 
 def test_bubble5(fullbh):
     fullbh._bubble(9)
-    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, -10, 104, 38, 55, -10, 30, 12, 78, 0, 8, 71]
+    assert fullbh._lst == SORTED
 
 
 def test_bubble6(fullbh):
     fullbh._bubble(10)
-    assert fullbh.lst == SORTED
-
-
-def test_bubble7(fullbh):
-    fullbh._bubble(-1)
-    assert fullbh.lst == SORTED
+    assert fullbh._lst == SORTED
 
 
 def test_sink1(emptybh):
     emptybh._sink(0)
-    assert emptybh.lst == []
+    assert emptybh._lst == []
 
 
 def test_sink2(emptybh):
-    emptybh.lst.append(1)
-    emptybh._sink()
-    assert emptybh.lst == [1]
+    emptybh._lst.append(1)
+    emptybh._sink(0)
+    assert emptybh._lst == [1]
 
 
 def test_sink3(fullbh):
-    fullbh.lst.append(-100)
-    fullbh._sink()
-    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+    fullbh._lst.append(-100)
+    fullbh._sink(len(fullbh._lst) - 1)
+    assert fullbh._lst == [-100, -99, -93, -38, -45, 38, -37, 12, -34, -10, 104, 68, 55, -10, 30, 45, 78, 39, 0, 71, 8]
 
 
 def test_sink4(fullbh):
-    fullbh.lst.append(0)
-    fullbh._sink()
-    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+    fullbh._lst.append(0)
+    fullbh._sink(len(fullbh._lst) - 1)
+    assert fullbh._lst == [-99, -45, -93, -38, -10, 38, -37, 12, -34, 0, 104, 68, 55, -10, 30, 45, 78, 39, 0, 71, 8]
 
 
 def test_sink5(fullbh):
-    fullbh.lst[6] = -100
+    fullbh._lst[6] = -100
     fullbh._sink(6)
-    assert fullbh.lst == [-99, -93, 45, -45, -38, 68, -37, 39, -34, 71, 104, 38, 55, -10, 30, 12, 78, 0, 8, -10]
+    assert fullbh._lst == [-100, -45, -99, -38, -10, 38, -93, 12, -34, 8, 104, 68, 55, -10, 30, 45, 78, 39, 0, 71]
 
 
 def test_pop1(emptybh):
@@ -130,8 +98,8 @@ def test_pop1(emptybh):
 
 def test_pop2():
     from src.binheap import BinHeap
-    binheap = BinHeap([5])
-    assert binheap.pop() == 5
+    bh = BinHeap([5])
+    assert bh.pop() == 5
 
 
 def test_pop3(fullbh):
@@ -139,34 +107,28 @@ def test_pop3(fullbh):
 
 
 def test_pop4(fullbh):
-    len_lst = len(fullbh.lst)
+    len_heap = len(fullbh._lst)
     fullbh.pop()
-    assert len(fullbh.lst) == len_lst - 1
+    assert len(fullbh._lst) == len_heap - 1
 
 
 def test_pop5(fullbh):
-    sorted_lst = LST
-    sorted_lst.sort()
+    sorted_heap = START
+    sorted_heap.sort()
     passes = True
-    for item in sorted_lst:
+    for item in sorted_heap:
         match = fullbh.pop()
-        assert item != match
-        # if item != match:
-        #     passes = False
-        #     break
-    # assert passes
+        if item != match:
+            passes = False
+            break
+    assert passes
 
 
 def test_push1(emptybh):
     emptybh.push(5)
-    assert len(emptybh.lst) == 1
+    assert len(emptybh._lst) == 1
 
 
 def test_push2(emptybh):
     emptybh.push(5)
-    assert emptybh.lst[0] == 5
-
-
-def test_push3(emptybh):
-    with pytest.raises(TypeError):
-        emptybh.push("hello")
+    assert emptybh._lst[0] == 5
