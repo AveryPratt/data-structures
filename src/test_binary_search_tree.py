c429bab5c1e95c92bb65e07c75af9ed574490535
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-16 14:18:26 -0800

    fixed tests to use fixture

--- src/test_binary_search_tree.py
+++ src/test_binary_search_tree.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+
 @pytest.fixture()
 def new_bst():
     """Fixture for a bst."""
@@ -9,36 +10,30 @@ def new_bst():
     bst = BinarySearchTree([8, 3, 10, 1, 6, 9])
     bst_empty = BinarySearchTree()
     bst_one_node = BinarySearchTree(3)
-    return bst
+    return bst, bst_empty, bst_one_node
 
 
 def test_initiate_node_data():
     """Test node data on init."""
     from binary_search_tree import Node
     node = Node(5)
-    assert Node.data == n
+    assert node.data == 5
 
 
 def test_initiate_node_children():
     """Test node children on init is None."""
     from binary_search_tree import Node
     node = Node(5)
-    assert Node.right == None
-    assert Node.left == None
+    assert node.right_child is None
+    assert node.left_child is None
 
 
-def test_initiate_bst():
+def test_initiate_bst(new_bst):
     """Test if bst is initiated has a root with data."""
-    from binary_search_tree import BinarySearchTree
-    bst = BinarySearchTree(3)
-    assert bst.root.data == 3
+    assert new_bst[2].root.data == 3
 
 
-def test_insert_on_an_empty_bst():
+def test_insert_on_an_empty_bst(new_bst):
     """Test insert on an empty bst updates the root."""
-    from binary_search_tree import BinarySearchTree
-    bst = BinarySearchTree()
-    bst.insert(5)
-    assert bst.root.data == 5
-
-
+    new_bst[1].insert(5)
+    assert new_bst[1].root.data == 5
