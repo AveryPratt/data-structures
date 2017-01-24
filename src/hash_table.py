5e480ccc6de19c4b4dc1d9f344fc68398db9897a
Avery Pratt <apratt91@gmail.com>
2017-01-23 17:59:12 -0800

    created hash table file and test file

---
+++ src/hash_table.py
@@ -0,0 +1,6 @@
+"""Contains a hash table with fixed number of buckets that can take different hash functions."""
+
+
+class HashTable(object):
+    """."""
+    pass
---
+++ src/test_hash_table.py
@@ -0,0 +1,6 @@
+"""Tests hash table."""
+
+
+from src.hash_table import HashTable
+
+
