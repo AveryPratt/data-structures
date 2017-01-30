542a376306168c68fdd65546384a701081f34eff
Avery Pratt <apratt91@gmail.com>
2017-01-30 14:52:00 -0800

    rough draft of insertion_sort (no tests)

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -1,6 +1,10 @@
 """Implementation of Insertion Sort."""
 
 
-def insertion_sort(lst):
-    """sorts input using insertion sort algorithm."""
-    pass
+def insertion_sort(input):
+    """sorts input using insertion sorting algorithm."""
+    for idx in range(len(input)):
+        while idx > 0 and input[idx] < input[idx - 1]:
+            input[idx], input[idx - 1] = input[idx - 1], input[idx]
+            idx -= 1
+    return input
