ede94a2fe3493c0246cc53695daa4508f0f6af14
Avery Pratt <apratt91@gmail.com>
2017-01-30 15:14:44 -0800

    added a test

--- src/test_insertion_sort.py
+++ src/test_insertion_sort.py
@@ -4,5 +4,7 @@
 import pytest
 
 
-def test_insertion_sort():
-    pass
+def test_insertion_sort_empty():
+    """Sort an empty list."""
+    from src.insertion_sort import insertion_sort
+    assert insertion_sort([]) == []
