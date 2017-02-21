e39f0a317611b575d1e4e080996fca6b6581715f
Avery Pratt <apratt91@gmail.com>
2017-02-21 14:45:14 -0800

    additional work on radix and quicksort

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -23,12 +23,12 @@ if __name__ == "__main__":
         "from random import Random\n" +
         "rand = Random()\n" +
         "pile_of_crap = []\n" +
-        "for i in range(10000):\n" +
-        "    pile_of_crap.append(rand.random())\n" +
+        "for i in range(1000000):\n" +
+        "    pile_of_crap.append(rand.randint(0, 1000000))\n" +
         "quick_sort(pile_of_crap)"
     )
     cmd2 = "quick_sort(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'])"
     timer = Timer(stmt=cmd1, setup="from __main__ import quick_sort")
     print("Sort 10000 random numbers: ", timer.timeit(1))
-    timer = Timer(stmt=cmd2, setup="from __main__ import quick_sort")
-    print("Sort qwerty alphabet 100000 times: ", timer.timeit(100000))
+    # timer = Timer(stmt=cmd2, setup="from __main__ import quick_sort")
+    # print("Sort qwerty alphabet 100000 times: ", timer.timeit(100000))
--- src/radix_sort.py
+++ src/radix_sort.py
@@ -6,20 +6,23 @@ def radix_sort(itr, base=10):
     divisor = base
     digit = 0
     try:
-        for item in range(max(itr) // divisor + 1):
-            buckets = []
-            for b in range(base):
-                buckets.append([])
-            for item in itr:
-                idx = item % divisor // base ** digit
-                buckets[idx].append(item)
-            itr = []
-            for bucket in buckets:
-                itr.extend(bucket)
-            divisor *= base
-            digit += 1
+        maximum = max(itr)
     except ValueError:
-        pass
+        return itr
+    while maximum > 1:
+        buckets = []
+        tmp = [num for num in itr]
+        for b in range(base):
+            buckets.append([])
+        for item in itr:
+            idx = item % divisor // base ** digit
+            buckets[idx].append(item)
+        itr = []
+        for bucket in buckets:
+            itr.extend(bucket)
+        divisor *= base
+        digit += 1
+        maximum /= float(base)
     return itr
 
 
@@ -56,13 +59,13 @@ if __name__ == "__main__":
         "pile_of_crap = []\n" +
         "for i in range(1000000):\n" +
         "    pile_of_crap.append(rand.randint(0, 1000000))\n" +
-        "radix_sort(pile_of_crap, 100000)"
+        "radix_sort(pile_of_crap, 10)"
     )
-    timer = Timer(stmt=cmd1, setup="from __main__ import radix_sort")
-    print("Sort 10,000 random numbers between 0 and 999 in binary (base 2): ", timer.timeit(1))
-    timer = Timer(stmt=cmd2, setup="from __main__ import radix_sort")
-    print("Sort 10,000 random numbers between 0 and 999 in decimal (base 10): ", timer.timeit(1))
-    timer = Timer(stmt=cmd3, setup="from __main__ import radix_sort")
-    print("Sort 10,000 random numbers between 0 and 999 in hexadecimal (base 16): ", timer.timeit(1))
+    # timer = Timer(stmt=cmd1, setup="from __main__ import radix_sort")
+    # print("Sort 10,000 random numbers between 0 and 999 in binary (base 2): ", timer.timeit(1))
+    # timer = Timer(stmt=cmd2, setup="from __main__ import radix_sort")
+    # print("Sort 10,000 random numbers between 0 and 999 in decimal (base 10): ", timer.timeit(1))
+    # timer = Timer(stmt=cmd3, setup="from __main__ import radix_sort")
+    # print("Sort 10,000 random numbers between 0 and 999 in hexadecimal (base 16): ", timer.timeit(1))
     timer = Timer(stmt=cmd4, setup="from __main__ import radix_sort")
     print("Sort 1,000,000 random numbers between 0 and 999,999 in base 100,000: ", timer.timeit(1))
