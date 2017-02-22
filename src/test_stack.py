923f5cecfb0bcb178c16bc8e80d49d838981925b
Avery Pratt <apratt91@gmail.com>
2017-02-22 00:04:37 -0800

    parametrized the fixture in test_stack.py

--- src/test_stack.py
+++ src/test_stack.py
@@ -4,51 +4,57 @@
 import pytest
 
 
-@pytest.fixture
-def st():
+ITER_TESTS = [
+    [1, 2, 3],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+    ["hi", "my", "name", "is", "bob"]
+]
+
+
+@pytest.fixture(params=ITER_TESTS)
+def st(request):
     """Create a fixture with a list as the value."""
     from stack import Stack
-    return Stack([1, 2, 3])
+    return Stack(request.param), request.param
 
 
-def test_init():
+def test_init(st):
     """Test creation of an empty stack."""
     from stack import Stack
-    st = Stack()
-    assert type(st) is Stack
+    assert type(st[0]) is Stack
 
 
 def test_with_iterable(st):
     """Test creation of a stack with an iterable."""
-    assert st._linked_list.head.data is 3
+    assert st[0]._linked_list.head.data is st[1][-1]
 
 
 def test_with_iterable2(st):
     """Test creation of a stack with an iterable."""
-    assert st._linked_list.head.next.data is 2
+    assert st[0]._linked_list.head.next.data is st[1][-2]
 
 
 def test_with_iterable3(st):
     """Test creation of a stack with an iterable."""
-    assert st._linked_list.head.next.next.data is 1
+    assert st[0]._linked_list.head.next.next.data is st[1][-3]
 
 
 def test_push_iterable1(st):
     """Test push method with iterable input."""
-    st.push(4)
-    assert st._linked_list.head.data is 4
+    st[0].push(4)
+    assert st[0]._linked_list.head.data is 4
 
 
 def test_push_iterable2(st):
     """Test push method with iterable input."""
-    st.push(4)
-    assert st._linked_list.head.next.data is 3
+    st[0].push(4)
+    assert st[0]._linked_list.head.next.data is st[1][-1]
 
 
 def test_pop(st):
     """Test that pop removes the top object."""
-    st.pop()
-    assert st._linked_list.head.data is 2
+    st[0].pop()
+    assert st[0]._linked_list.head.data is st[1][-2]
 
 
 def test_pop_empty():
@@ -60,4 +66,4 @@ def test_pop_empty():
 
 def test_pop_return(st):
     """Test that pop returns the right value."""
-    assert st.pop() is 3
+    assert st[0].pop() is st[1][-1]
